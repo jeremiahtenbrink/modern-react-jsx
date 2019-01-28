67e334520caa9f6ee7299c0cd5d87fe666475a94
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-01-27 17:17:34 -0700

    created a label form in jsx and displayed it on the html page. Added a config file for spacing that works with eslint airbnb

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const App = () => (
+    <div>
+        <label htmlFor="name">Enter Name:</label>
+        <input id="name" type="text" />
+        <button style={ { backgroundColor: "blue", color: "white" } } >Submit </button>
+    </div>
+);
 
+ReactDOM.render(
+    <App />,
+    document.querySelector( "#root" ),
+);
